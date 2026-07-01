6c10427338320dc6723a0253fac42892ecad72e8
Nirjal Acharya <acharyanirjal7@gmail.com>
2026-07-01 21:36:59 +0545

    register removed and login for admin fixed

--- routes/web.php
+++ routes/web.php
@@ -72,9 +72,9 @@ require __DIR__.'/auth.php';
 */
 
 Route::middleware('auth')->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    // Route::get('/dashboard', function () {
+    //    return view('dashboard');
+    // })->name('dashboard');
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
